b1331aa5ea40cb38a79aef271a8de5e13bc85a76
TheRomanian9d <97812866+TheRomanian9d@users.noreply.github.com>
2022-01-27 16:30:42 +0000

    fixed letter 'H' pixels, added comments and rainbow underline to second case
    also made underlining function to avoid code repetition and improved prompt message when choosing animation

--- Fadecandy_2400-main/assignment.py
+++ Fadecandy_2400-main/assignment.py
@@ -8,6 +8,8 @@ import colorsys
 client = opc.Client('localhost:7890') #connects code to LED emulator
 
 led_colour=[(0,0,0)]*360 #sets a blank screen
+s = 1.0 #used to set maximum colour to hsv chart
+v = 1.0 #used to set maximum brightness to hsv chart
 
 def print_letters(text): #function to print input letters to the LED emulator
     global led_colour
@@ -29,6 +31,14 @@ def print_letters(text): #function to print input letters to the LED emulator
 
     client.put_pixels(led_colour) #prints LED pixels to emulator
 
+def draw_undreline(colour,sleep_time): #function to draw an underline on the bottom line of the emulator
+    led = 60*5 #used to ignore the first 5 lines
+    while led < 60*6:
+        led_colour[led] = colour #each pixel in line 6 is chosen
+        client.put_pixels(led_colour) #colour is sent to the pixel
+        time.sleep(sleep_time)
+        led +=1 #increase pixel number
+
 
 
 def animation_choice(number):
@@ -41,25 +51,30 @@ def animation_choice(number):
             print_letters([p,e,w,space,p,e,w])
             time.sleep(1)
             #creates a moving underline animation
-            led = 60*5
-            while led < 60*6:
-                led_colour[led] = (255,0,0)
-                client.put_pixels(led_colour)
-                time.sleep(0.1)
-                led +=1
+            draw_undreline((255,0,0),0.1)
 
         case 2: #prints user's name
             name = input("What is your name?\n")
-            if len(name) < 10:
+            if len(name) < 10: #check to see if name is too long
                 chars = []
                 for letter in name:
-                    if letter.isalpha():
-                        chars.append(globals()[letter])
+                    if letter.isalpha(): #check to see if only letters are inputted
+                        chars.append(globals()[letter]) #set list with characters from input
                     else:
                         print("Name contains unrecognised characters")
                 print_letters(chars)
                 time.sleep(1)
-
+                draw_undreline((255,0,0),0.1)
+                for hue in range(360): #convert rgb to hsv
+                    rgb_fractional = colorsys.hsv_to_rgb(hue/360.0, s, v) #get float between 1 and 0
+                    #assign floats to rgb variants
+                    r_float = rgb_fractional[0]
+                    g_float = rgb_fractional[1]
+                    b_float = rgb_fractional[2]
+                    rgb = (r_float*255, g_float*255, b_float*255) #get tupple with real rgb vlues from variants
+                    print(rgb) #debug
+                    draw_undreline(rgb,0) #set colour to underline
+                    time.sleep(0.03) #set speed of hue transition
             else:
                 print("Name is too long")
         case 3:
@@ -71,4 +86,5 @@ def animation_choice(number):
         case _:
             print("Option not recognised")
 
-animation_choice(2)
+choice = int(input("Which animation would you like to see?\n1. Author's intro\n2. Print your name\n\ntype number:"))
+animation_choice(choice)
